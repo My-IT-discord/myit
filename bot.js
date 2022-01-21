0d5ad342e2314ff2cf56e527f740076ff98ffd14
a3510377 <a102009102009@gmail.com>
2022-01-21 22:49:55 +0800

    debug

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import fs from "fs";
 import { config } from "dotenv";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const bulletin = require("./data/bulletin.json");
 
 config({ path: ".env.local" });
 const Token = process.env.TOKEN;
@@ -25,28 +28,29 @@ const limit = 5;
   );
   roles = Object.fromEntries(roles.map((v) => [v.id, v]));
   channels = Object.fromEntries(channels.map((v) => [v.id, v]));
-
-  fs.writeFile(
-    "data/bulletin.json",
-    JSON.stringify(
-      messages.map((msg) => {
-        let msg_roles = {};
-        msg.mention_roles?.forEach(
-          (role) => role in roles && (msg_roles[role] = roles[role])
-        );
-        msg.mention_channels ||= {};
-        (msg.content.match(/<#[0-9]+>/gm) || [])
-          .map((v) => v.match(/[0-9]+/)?.[0])
-          .filter((v) => v)
-          .forEach(
-            (channelId) =>
-              channelId in channels &&
-              (msg.mention_channels[channelId] = channels[channelId])
-          );
-        msg.mention_roles = msg_roles;
-        return msg;
-      })
-    ),
-    (e) => console.log(e)
+  let newData = messages.map((msg) => {
+    let msg_roles = {};
+    msg.mention_roles?.forEach(
+      (role) => role in roles && (msg_roles[role] = roles[role])
+    );
+    msg.mention_channels ||= {};
+    (msg.content.match(/<#[0-9]+>/gm) || [])
+      .map((v) => v.match(/[0-9]+/)?.[0])
+      .filter((v) => v)
+      .forEach(
+        (channelId) =>
+          channelId in channels &&
+          (msg.mention_channels[channelId] = channels[channelId])
+      );
+    msg.mention_roles = msg_roles;
+    return msg;
+  });
+  if (
+    bulletin[0].timestamp === newData[0].timestamp &&
+    bulletin[0].edited_timestamp === newData[0].edited_timestamp
+  )
+    return;
+  fs.writeFile("data/bulletin.json", JSON.stringify(newData), (e) =>
+    console.log(e)
   );
 })();
