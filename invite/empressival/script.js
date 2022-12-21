97a04f2cbb5899de42f1eb717959295d59d194a1
Hello Phone <85028651+hello1234316@users.noreply.github.com>
2022-12-21 15:40:38 +0800

    Delete invite directory

--- invite/empressival/script.js
+++
@@ -1,8 +0,0 @@
-// invite
-const url = "https://discord.com/oauth2/authorize?client_id=942082423846486056&permissions=8&scope=bot%20applications.commands";
-
-let linkClicker = document.getElementById('link_click');
-
-window.onload = window.onfocus = () => {
-  this.document.location.href = url;
-}
