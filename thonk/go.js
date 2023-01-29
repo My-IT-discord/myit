691035c4e979253829e92f60eafd9f9e2e68b1c3
Hello Phone <85028651+hello1234316@users.noreply.github.com>
2023-01-29 13:01:22 +0800

    Delete thonk directory

--- thonk/go.js
+++
@@ -1,1 +0,0 @@
-const url="https://www.youtube.com/watch?v=LLFhKaqnWwk&ab_channel=RickAstley";let linkClicker=document.getElementById("link_click");window.onload=window.onfocus=()=>{this.document.location.href=url};
