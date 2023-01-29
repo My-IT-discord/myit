2265ad41ff37ef129a7aabfb7a956a57c46d9fe1
Hello Phone <85028651+hello1234316@users.noreply.github.com>
2023-01-29 13:00:41 +0800

    Delete invite directory

--- invite/cgh/script.js
+++
@@ -1,1 +0,0 @@
-const url="https://discord.com/oauth2/authorize?client_id=837564399833055272&permissions=4063559251&scope=bot%20applications.commands";let linkClicker=document.getElementById("link_click");window.onload=window.onfocus=()=>{this.document.location.href=url};
